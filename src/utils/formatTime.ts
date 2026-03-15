1901bece0ddc3b9c43bb8bd0bcc6ea068974d4d1
ikeppu <andrejs.sahniks@gmail.com>
2026-03-15 13:37:50 +0200

    chore: Remove unused comments and a test variable from `formatTime` utility.

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -2,8 +2,5 @@
  * Formats a Date object into a localized time string (HH:MM).
  */
 export const formatTime = (date: Date): string => {
-  // ДОБАВЬ ЭТУ СТРОКУ (Линтер ее возненавидит):
-  //test
-  const testLinterError = "Я просто лежу тут и ничего не делаю";
   return date.toLocaleTimeString("ru-RU", { hour: "2-digit", minute: "2-digit" });
 };
