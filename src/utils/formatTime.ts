88eb2601d8bece0e8d447e8b5dedfe404a45f4ef
ikeppu <andrejs.sahniks@gmail.com>
2026-03-15 13:36:31 +0200

    refactor: update time formatting logic in `formatTime.ts` utility.

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -3,6 +3,7 @@
  */
 export const formatTime = (date: Date): string => {
   // ДОБАВЬ ЭТУ СТРОКУ (Линтер ее возненавидит):
+  //test
   const testLinterError = "Я просто лежу тут и ничего не делаю";
   return date.toLocaleTimeString("ru-RU", { hour: "2-digit", minute: "2-digit" });
 };
