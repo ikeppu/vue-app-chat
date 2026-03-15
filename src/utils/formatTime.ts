fe253c602b18d39dfc18cd48a6ae233b4b09b602
ikeppu <andrejs.sahniks@gmail.com>
2026-03-15 13:30:59 +0200

    style: remove unnecessary comment from `formatTime` function.

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -2,6 +2,7 @@
  * Formats a Date object into a localized time string (HH:MM).
  */
 export const formatTime = (date: Date): string => {
-  // asdas
+  // ДОБАВЬ ЭТУ СТРОКУ (Линтер ее возненавидит):
+  const testLinterError = "Я просто лежу тут и ничего не делаю";
   return date.toLocaleTimeString("ru-RU", { hour: "2-digit", minute: "2-digit" });
 };
